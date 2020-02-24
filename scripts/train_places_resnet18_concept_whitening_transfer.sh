4a8936dc2d15b583eae1fffda2937912f696fdee
Zhi Chen <zhichen@cs.duke.edu>
2020-02-23 23:29:15 -0500

    reorgnize and clean up the code

--- scripts/train_places_resnet18_concept_whitening_transfer.sh
+++ scripts/train_places_resnet18_concept_whitening_transfer.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-python3 ../train_places.py --ngpu 1 --workers 4 --arch resnet_cw --depth 18 --epochs 200 --batch-size 64 --lr 0.05 --whitened_layers 5 --concepts airplane,bed,person --prefix RESNET18_PLACES365_CPT_WHITEN_TRANSFER /usr/xtmp/zhichen/data_256/
+#!/bin/sh
+python3 ../train_places.py --ngpu 1 --workers 4 --arch resnet_cw --depth 18 --epochs 200 --batch-size 64 --lr 0.05 --whitened_layers 1 --concepts airplane,bed,person --prefix RESNET18_PLACES365_CPT_WHITEN_TRANSFER /usr/xtmp/zhichen/data_256/
