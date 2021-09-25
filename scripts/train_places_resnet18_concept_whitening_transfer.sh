a369fb4334cca59f2cc97b72a33df6b9a9903bd8
zhiCHEN96 <zc72@duke.edu>
2021-09-24 23:41:14 -0400

    Update train_places_resnet18_concept_whitening_transfer.sh
    change the concepts to match the testing script

--- scripts/train_places_resnet18_concept_whitening_transfer.sh
+++ scripts/train_places_resnet18_concept_whitening_transfer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 ../train_places.py --ngpu 1 --workers 4 --arch resnet_cw --depth 18 --epochs 200 --batch-size 64 --lr 0.05 --whitened_layers 7 --concepts bicycle,bird,boat,cat,chair,knife,microwave,pizza,skis --prefix RESNET18_PLACES365_CPT_WHITEN_TRANSFER /usr/xtmp/zhichen/data_256/
+python3 ../train_places.py --ngpu 1 --workers 4 --arch resnet_cw --depth 18 --epochs 200 --batch-size 64 --lr 0.05 --whitened_layers 8 --concepts airplane,bed,bench,boat,book,horse,person --prefix RESNET18_PLACES365_CPT_WHITEN_TRANSFER /usr/xtmp/zhichen/data_256/
