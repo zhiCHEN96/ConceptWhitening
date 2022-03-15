2517fc3841d688fa129ca22ebe234a1b984ec54d
Zhi Chen <zhichen@cs.duke.edu>
2022-03-15 15:44:31 -0400

    add ISIC prepocessing code and clean up

--- scripts/train_isic_resnet18_concept_whitening_transfer.sh
+++ scripts/train_isic_resnet18_concept_whitening_transfer.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-#SBATCH --gres=gpu:1
-#SBATCH --partition=compsci-gpu
-
 python3 ../train_isic.py --ngpu 1 --workers 4 --arch resnet_cw --depth 18 --epochs 10 --batch-size 32 --lr 0.005 --whitened_layers 8 --concepts age_le_20,size_geeq_10 --prefix resnet18_isic /usr/xtmp/zhichen/ISIC_data/
--- scripts/train_isic_resnet18_original.sh
+++ scripts/train_isic_resnet18_original.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-#SBATCH --gres=gpu:1
-#SBATCH --partition=compsci-gpu
-
 python3 ../train_isic.py --ngpu 1 --workers 4 --arch resnet_original --depth 18 --epochs 30 --batch-size 64 --lr 0.005 --prefix resnet18_isic /usr/xtmp/zhichen/ISIC_data/
